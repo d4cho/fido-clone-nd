97a681acdb8b38c321db159b3cf228212e7892f9
Nav <navdeep.dhamrait@thebioagency.com>
2022-12-15 11:27:53 -0500

    added billsandpayment page and pdf viewer functionality

--- src/Components/Views/AccountPage.jsx
+++ src/Components/Views/AccountPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../Atoms/Title/Title';
 import BreadCrumbsContent from '../Organisms/BreadCrumbsContent/BreadCrumbsContent';
 import { useContext } from 'react';
@@ -6,9 +6,13 @@ import { MainContext } from '../../Context/MainContext';
 import { breadCrumbsLabelsForAccount } from '../../Utils/NavbarLabel';
 import MyAccountContent from '../Organisms/MyAccountContent/MyAccountContent';
 import { accountOverviewLabels } from '../../Utils/AccountOverviewLabels';
+import AlertContent from '../Molecules/AlertContent/AlertContent';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import CloseIcon from '@mui/icons-material/Close';
 
 function AccountPage() {
     const { matches } = useContext(MainContext);
+    const [showAlert, setShowAlert] = useState(true);
     return (
         <div style={{ backgroundColor: '#e9e9e9', paddingBottom: '90px' }}>
             <BreadCrumbsContent data={breadCrumbsLabelsForAccount} />
@@ -19,6 +23,47 @@ function AccountPage() {
                     fontWeight='700'
                     marginTop='50px'
                     marginLeft={!matches ? '20px' : null}
+                />
+                <AlertContent
+                    backgroundColor='#effdff'
+                    marginBottom='20px'
+                    content={
+                        <div
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'flex-start',
+                                flexDirection: 'column',
+                                gap: '5px',
+                                marginLeft: '20px',
+                            }}
+                        >
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    gap: '5px',
+                                }}
+                            >
+                                <ErrorOutlineIcon />
+                                <b> Psst. Exclusive offer right here</b>
+                            </div>
+                            Get a new phone today & save with this extra special deal. Just for you.
+                        </div>
+                    }
+                    closeIcon={
+                        <div
+                            style={{
+                                position: 'absolute',
+                                bottom: '30px',
+                                right: '20px',
+                                top: '10px',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <CloseIcon onClick={() => setShowAlert(false)} />
+                        </div>
+                    }
+                    show={showAlert}
                 />
                 <MyAccountContent accountOverviewLabel={accountOverviewLabels} />
             </div>
--- src/Components/Views/Bill&PaymentPage.jsx
+++ src/Components/Views/Bill&PaymentPage.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import Title from '../Atoms/Title/Title';
 import BreadCrumbsContent from '../Organisms/BreadCrumbsContent/BreadCrumbsContent';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { MainContext } from '../../Context/MainContext';
 import { breadCrumbsLabelsForBillAndPayment } from '../../Utils/NavbarLabel';
 import BillAndPaymentContent from '../Organisms/Bill&PaymentContent/Bill&PaymentContent';
 import { accountOverviewLabels } from '../../Utils/AccountOverviewLabels';
+import PictureAsPdfOutlinedIcon from '@mui/icons-material/PictureAsPdfOutlined';
+import Button from '../Atoms/Button/Button';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
+
 
 function BillAndPaymentPage() {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [show, setShow] = useState(false);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1);
+    }
+
+    const showPdf = () => {
+        if (show) {
+            onDocumentLoadSuccess();
+        }
+        setShow(true);
+    };
+
+    function changePage(offSet) {
+        setPageNumber((prevPageNumber) => prevPageNumber + offSet);
+    }
+
+    function changePageBack() {
+        changePage(-1);
+    }
+
+    function changePageNext() {
+        changePage(+1);
+    }
     const { matches } = useContext(MainContext);
     return (
         <div style={{ backgroundColor: '#e9e9e9', paddingBottom: '90px' }}>
@@ -22,6 +53,37 @@ function BillAndPaymentPage() {
                     marginLeft={!matches ? '20px' : null}
                 />
                 <BillAndPaymentContent accountOverviewLabel={accountOverviewLabels} />
+                <Button
+                    title='View Bill'
+                    backgroundColor='#e9e9e9'
+                    border='none'
+                    marginTop='10px'
+                    color='teal'
+                    filterIcon={<PictureAsPdfOutlinedIcon />}
+                    onClick={showPdf}
+                />
+                {show && matches ? (
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Document file='Fido-2022.pdf' onLoadSuccess={onDocumentLoadSuccess}>
+                            <Page height={600} pageNumber={pageNumber} />
+                        </Document>
+                    </div>
+                ) : null}
+
+                {pageNumber > 1 && matches && show ? (
+                    <button onClick={changePageBack}>Previous Page</button>
+                ) : null}
+                {pageNumber < numPages && matches && show ? (
+                    <button onClick={changePageNext}>Next Page</button>
+                ) : null}
+
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    {show && matches ? (
+                        <p>
+                            Page {pageNumber} of {numPages}
+                        </p>
+                    ) : null}
+                </div>
             </div>
         </div>
     );
