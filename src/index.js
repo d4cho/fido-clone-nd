5dd37ce9a3da828a576728d4610492a0a2ad2ffa
d4cho <danielcho91@gmail.com>
2023-11-24 10:41:30 -0500

    add bio widget

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import MainContextProvider from './Context/MainContext';
-import FilterContextProvider from './Context/FilterContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import MainContextProvider from "./Context/MainContext";
+import FilterContextProvider from "./Context/FilterContext";
+import BioStorybookWidget from "bio-storybook-widget";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <MainContextProvider>
-                <FilterContextProvider>
-                    {' '}
-                    <App />
-                </FilterContextProvider>
-            </MainContextProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BioStorybookWidget projectSource={"fido"} />
+    <BrowserRouter>
+      <MainContextProvider>
+        <FilterContextProvider>
+          {" "}
+          <App />
+        </FilterContextProvider>
+      </MainContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
